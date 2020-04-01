05766e26e78d146547f424103688d2169608feb5
kiands <794583439@qq.com>
2020-04-01 10:03:21 +0800

    Create launch.php
    User can input amount to donate. This page will automatically generate a n order number then post them together to the next page called ebpay.php

---
+++ launch.php
@@ -0,0 +1,15 @@
+<?php
+	$strs="QWERTYUIOPASDFGHJKLZXCVBNM1234567890qwertyuiopasdfghjklzxcvbnm";
+	$random=substr(str_shuffle($strs),mt_rand(0,strlen($strs)-11),6);
+	$OrderNo = $random.'at'.time();
+	
+	echo"
+	<form name='Newebpay' method='post' action='http://www.pt.com/ebpay.php'>
+		sis_bro_name:<input type='text' name='sis_bro_name' value=''>
+		<br></br>
+		amount:<input type='text' name='amount' value=''>
+		<br></br>
+		OrderNo:<input type='hidden' name='OrderNo' value='$OrderNo'>
+		<br></br>
+		<input type='submit' value='Submit'>
+	</form>";
